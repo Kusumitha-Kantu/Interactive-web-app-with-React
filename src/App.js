2a71fbc691019f7a9a8675b6b4711306038f3ba5
Kusumitha Kantu <kusumithakantu@gmail.com>
2024-03-23 23:52:11 +0530

    Adding Header and CoreConcepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './assets/react-core-concepts.png';
+// import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} alt="logo" className='App-logo'/>
+//         <h> React Essentials</h>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+import Header from "./components/Header";
+import {CORE_CONCEPTS} from './data.js';
+import CoreConcept from './components/CoreConcept.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <Header />
+        <main>
+          <section id = "core-concepts">
+            <h2>Core Concepts</h2>
+            <ul>
+              <CoreConcept 
+                image = {CORE_CONCEPTS[0].image}
+                title = {CORE_CONCEPTS[0].title}
+                description = {CORE_CONCEPTS[0].description}/>
+              <CoreConcept {...CORE_CONCEPTS[1]}/>
+              <CoreConcept {...CORE_CONCEPTS[2]}/>
+              <CoreConcept {...CORE_CONCEPTS[3]}/>
+            </ul>
+          </section>
+        </main>
+      </div>
   );
 }
 
 export default App;
+
